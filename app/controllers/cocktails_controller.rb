1c51b87bcbc88a771f718a3071be03b417302538
Noah Schlesinger <noah.njs@gmail.com>
2019-11-22 11:45:54 +0200

    still completeting things

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -22,7 +22,7 @@ class CocktailsController < ApplicationController
 
   def destroy
     @cocktail = Cocktail.find(params[:id])
-    @cocktail.delete
+    @cocktail.destroy
     redirect_to cocktails_path
   end
 
--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,5 +1,5 @@
 class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
-  validates :cocktail, uniqueness: { scope: :ingredient }
+  validates :cocktail_id, uniqueness: { scope: :ingredient_id }
 end
